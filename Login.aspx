228f377fcc3facfc507715d08b45c90cc0134f8f
parmu <mulla.parvez@gmail.com>
2017-04-04 23:54:25 -0500

    First commit

---
+++ Login.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Login.aspx.cs" Inherits="Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>CHARTWELLS</title>
+    <link href="CSS/StyleSheet.css" rel="stylesheet" />
+    
+</head>
+<body>
+  
+    <div id="container">
+         <div id="logo">
+             <img src="images/TAMU.png" style="background-color:#5d0f0f" />
+         </div>
+        <br />
+        <br />
+        <div class="login-page">
+          <div class="form">
+              <img src="Images/chartwells-logo.png" style="width:250px; padding-bottom:20px" />  
+                  <form class="login-form" runat="server">
+                     <asp:TextBox ID="txtEmail" runat="server" placeholder="E-Mail ID" ></asp:TextBox>
+                       <asp:TextBox ID="txtPassword" runat="server" placeholder="Password" TextMode="Password"></asp:TextBox>
+                      <asp:Button ID="btnLogin" runat="server" Text="LOGIN" OnClick="btnLogin_Click" />
+                      <asp:RegularExpressionValidator ID="RegularExpressionValidator1" Display="Dynamic" runat="server" ErrorMessage="Invalid Email ID" ControlToValidate="txtEmail" ValidationExpression="\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*" ForeColor="red"></asp:RegularExpressionValidator>
+                      </form>
+          </div>
+        </div>
+    </div>
+    
+</body>
+</html>
---
+++ Login.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        string emailid = txtEmail.Text;
+        string pwd = txtPassword.Text;
+        if(emailid=="" | pwd == "")
+        {
+            ClientScript.RegisterStartupScript(Page.GetType(), "validation", "<script language='javascript'>alert('Please enter your credentials !!!')</script>");
+        }
+        string connstring = ConfigurationManager.ConnectionStrings["login"].ConnectionString;
+        SqlConnection conn = new SqlConnection(connstring);
+        conn.Open();
+        string query = "select * from admin where email ='"+emailid+"' and password='"+pwd+"'" ;
+        SqlCommand cmd = new SqlCommand(query, conn);
+        SqlDataReader dr = cmd.ExecuteReader();
+        if(dr.HasRows)
+             {
+                conn.Close();
+                Session["username"] = "admin";
+                Response.Redirect("home.aspx");
+             }
+        else
+            {
+             ClientScript.RegisterStartupScript(Page.GetType(), "validation", "<script language='javascript'>alert('Invalid Username and Password')</script>");
+           
+        }
+    }
+}
